d643b805e2dfeefbcd9af2e4e844540c693c82ea
James Tuazon <jchristiantuazon@gmail.com>
2026-07-16 15:02:20 -0700

    Added in game timer, worked on checkpoints
    Checkpoints now only trigger once. In game timer displayed on top left corner of screen.

--- Scripts/canvas_layer.gd
+++ Scripts/canvas_layer.gd
@@ -1,9 +1,31 @@
 extends CanvasLayer
 
 var score = 0
-
 @onready var score_label: Label = $ScoreLabel
+
+var time_elapsed: float = 0.0
+@onready var timer_label: Label = $TimerLabel
+var is_timer_active: bool = true
 
 func add_point():
 	score += 1
 	score_label.text = str(score)
+
+
+func _process(delta: float) -> void:
+	if is_timer_active:
+		time_elapsed += delta
+		timer_label.text = format_time(time_elapsed)
+
+func format_time(time: float) -> String:
+	var minutes: int = int(time / 60)
+	var seconds: int = int(time) % 60
+	# Multiply remainder by 100 to get centiseconds (hundredths of a second)
+	var centiseconds: int = int((time - int(time)) * 100)
+	
+	# If under a minute, you can show just SS.CC (e.g., "04.25")
+	# If over a minute, display MM:SS.CC (e.g., "01:05.12")
+	if minutes > 0:
+		return "%02d:%02d.%02d" % [minutes, seconds, centiseconds]
+	else:
+		return "%02d.%02d" % [seconds, centiseconds]
--- Scripts/check_point.gd
+++ Scripts/check_point.gd
@@ -1,9 +1,18 @@
 extends Area2D
 
 #@onready var checkpoint_manager: Node = %CheckpointManager
+var is_activated: bool = false
+
+
 
 func _on_body_entered(body: Node2D) -> void:
 	#checkpoint_manager.last_location = $RespawnPoint.global_position
+	
+	if is_activated or body.name != "Player":
+		return
+	
+	is_activated = true
+	
 	if body.name == "Player":
 		# Update the global Autoload directly
 		CheckpointManager.last_location = $RespawnPoint.global_position
