22cb5ef10cd0983c608d52f5c9e064ba5e6c4054
James Tuazon <jchristiantuazon@gmail.com>
2026-07-18 15:18:05 -0700

    Added ice physics
    Specific tiles have ice physics

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -5,14 +5,16 @@ const SPEED = 130.0
 const JUMP_VELOCITY = -300.0
 
 @onready var animated_sprite: AnimatedSprite2D = $AnimatedSprite2D
+@onready var floor_ray_cast: RayCast2D = $floorRayCast
 
-#Test
-#var is_dead: bool = false
+#for faster acceleration increase val
+@export var accelerationVal = 0.01
+# for longer sliding time reduce value
+@export var slideValue = 0.01
+@export var fullStopValue = 15
+
 
 func _physics_process(delta: float) -> void:
-	#Test
-	#if is_dead:
-		#return # Stop processing ALL gravity, animations, and movement
 	
 	# Add the gravity.
 	if not is_on_floor():
@@ -42,9 +44,48 @@ func _physics_process(delta: float) -> void:
 	else:
 		animated_sprite.play("jump")
 	
+	#on ice
+	if _is_on_ice():
+		_movement_on_ice(direction)
+	else:
+		_normal_movement(direction)
+	
+	move_and_slide()
+
+func _movement_on_ice(direction):
+	if direction:
+		velocity.x = lerp(velocity.x, direction * SPEED, accelerationVal)
+	else:
+		velocity.x = lerp(velocity.x, 0.0, slideValue)
+		
+		if velocity.x < fullStopValue and velocity.x > -fullStopValue:
+			velocity.x = 0
+		
+
+func _normal_movement(direction):
 	if direction:
 		velocity.x = direction * SPEED
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 
-	move_and_slide()
+func _is_on_ice() -> bool:
+	if not floor_ray_cast.is_colliding():
+		return false
+	
+	var collider = floor_ray_cast.get_collider()
+	
+	if collider is TileMapLayer:
+	# Get the exact global coordinates where the raycast hit
+		var collision_point = floor_ray_cast.get_collision_point()
+		# Adjust slightly inward along the ray direction to ensure we target the right cell
+		var local_point = collider.to_local(collision_point + floor_ray_cast.target_position.normalized() * 0.1)
+		var cell_coords = collider.local_to_map(local_point)
+		
+		# Get the data container for that specific tile
+		var tile_data = collider.get_cell_tile_data(cell_coords)
+		
+		if tile_data:
+			# Read the custom data layer we created
+			return tile_data.get_custom_data("is_ice")
+			
+	return false
